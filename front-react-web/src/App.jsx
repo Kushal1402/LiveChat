9c19542191631131cbc2019c11ea8ce437c3aa99
Kushal1402 <kushalhemant2003@gmail.com>
2025-04-13 01:23:43 +0530

    chore: downgrade React and ReactDOM to version 18.3.1  for emoji-mart compatibility; add LoadingScreen component for lazy loading fallback

--- front-react-web/src/App.jsx
+++ front-react-web/src/App.jsx
@@ -1,8 +1,11 @@
 import React, { lazy, Suspense } from "react"
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 import { Toaster } from "@/components/ui/toaster"
 import { AuthRoute, OTPVerificationRoute, ProtectedRoute } from "./routes/Guards";
 import { useSelector } from "react-redux";
+
+import LoadingScreen from "./components/ui/LoadingScreen";
 
 const Login = lazy(() => import("./app/login/Login"));
 const ChatWindow = lazy(() => import("./app/Chat/Page"));
@@ -10,8 +13,6 @@ const OTPVerification = lazy(() => import("./app/login/OTPVerification"));
 const RegisterForm = lazy(() => import("./app/login/RegisterForm"));
 const ForgotPassword = lazy(() => import("./app/login/ForgotPassword"));
 const ResetPassword = lazy(() => import("./app/login/ResetPassword"));
-
-
 
 function App() {
 
@@ -22,25 +23,28 @@ function App() {
   return (
     <>
       <Toaster />
+
       <BrowserRouter>
-        <Routes>
-          <Route element={<AuthRoute />}>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<RegisterForm />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
-            <Route path="/reset-password" element={<ResetPassword />} />
-          </Route>
+        <Suspense fallback={<LoadingScreen />}>
+          <Routes>
+            <Route element={<AuthRoute />}>
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<RegisterForm />} />
+              <Route path="/forgot-password" element={<ForgotPassword />} />
+              <Route path="/reset-password" element={<ResetPassword />} />
+            </Route>
 
-          <Route element={<OTPVerificationRoute />}>
-            <Route path="/otp-verification" element={<OTPVerification />} />
-          </Route>
+            <Route element={<OTPVerificationRoute />}>
+              <Route path="/otp-verification" element={<OTPVerification />} />
+            </Route>
 
-          <Route element={<ProtectedRoute />}>
-            <Route path="/chat" element={<ChatWindow />} />
-          </Route>
+            <Route element={<ProtectedRoute />}>
+              <Route path="/chat" element={<ChatWindow />} />
+            </Route>
 
-          <Route path="/" element={<Navigate to="/login" replace />} />
-        </Routes>
+            <Route path="/" element={<Navigate to="/login" replace />} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
 
     </>
