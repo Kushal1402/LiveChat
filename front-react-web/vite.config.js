6a56b4d015638f32aa6649dc0edb1bdb7efbd953
Kushal1402 <kushalhemant2003@gmail.com>
2025-04-13 12:31:47 +0530

    fix: remove base path from Vite configuration

--- front-react-web/vite.config.js
+++ front-react-web/vite.config.js
@@ -11,7 +11,6 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/",
 
   // optimizeDeps: {
   //   include: ["@emoji-mart/react", "@emoji-mart/data"],
