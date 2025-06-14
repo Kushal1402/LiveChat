e749051c648bbcb6a67b316e4df2cc3252bf60b1
Kushal Doshi <kushalhemant2003@gmail.com>
2025-06-14 18:36:47 +0000

    fix: update Redis client configuration

--- front-node-api/utils/redis.js
+++ front-node-api/utils/redis.js
@@ -5,8 +5,8 @@ const redisClient = createClient({
     username: 'default',
     password: process.env.REDIS_PASSWORD,
     socket: {
-        host: 'redis-16398.crce182.ap-south-1-1.ec2.redns.redis-cloud.com',
-        port: 16398
+        host: process.env.REDIS_HOST,
+        port: parseInt(process.env.REDIS_PORT),
     }
 });
 
